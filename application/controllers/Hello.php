27fd61e01025c64f6dbaa5e0dac8c3116ecdaca4
raynaldi262 <ginantaraaldi1387@gmail.co.id>
2022-05-19 11:29:52 +0700

    First commit

---
+++ application/controllers/Hello.php
@@ -0,0 +1,16 @@
+<?php 
+class Hello extends CI_Controller{
+
+    public function index(){
+        
+        echo "Hello world";
+        // $this->load->view('contoh_view');
+
+    }
+
+    public function tes(){
+        echo "Tes2";
+    }
+}
+
+?>
---
+++ application/controllers/products/Product.php
@@ -0,0 +1,8 @@
+<?php 
+class Product extends CI_Controller{
+    public function index () : void{
+        echo "mskk pak ekoo";
+    }
+}
+
+?>
